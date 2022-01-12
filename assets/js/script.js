74d714722883079ab83bccbc3eead12813efbe5d
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-01-11 19:02:36 -0800

    completed event listener in js file to retrieve appropriate weather stats from the city entered in by user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ let searchBtnEl = document.querySelector("#search-btn");
 
 let weatherRequestUrl = 'http://api.openweathermap.org/data/2.5/weather';
 let apiKey = "f35594544b9f9597df35098b60602c39";
-let city;
-
 
 
 
@@ -20,15 +18,17 @@ let city;
 
       let res = await response.json();
 
+
       let temp = res.main.temp;
       let humidity = res.main.humidity;
       let wind = res.wind.speed
       let icon = `https://openweathermap.org/img/w/${res.weather[0].icon}.png`;
 
+      
+
       getUVI(res.coord.lat, res.coord.lon);
   }
 
-  currentWeather(`denver`);
 
 
   async function getUVI(lat, lon) {
@@ -44,6 +44,6 @@ let city;
 
 // Event Listeners
 
-  searchBtnEl.addEventListener("click",function (params) {
-    console.log("Hello World");
+  searchBtnEl.addEventListener("click",function() {
+    currentWeather(document.querySelector("#city-input").value);
   })
