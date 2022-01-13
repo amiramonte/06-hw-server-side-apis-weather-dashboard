1be5654b07447924e2a5f3eb962c58ab7ad01e57
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-01-12 16:16:31 -0800

    added styling to css file margin bottom to search bar, added function and api call for 5 day forecast in js file, added for loop to extract appropriate forecast information, dynamically created forecast cards and updated them each with the appropriate weather stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,13 +28,19 @@ let apiKey = "f35594544b9f9597df35098b60602c39";
       let wind = res.wind.speed
       let icon = `https://openweathermap.org/img/w/${res.weather[0].icon}.png`;
 
+      let img = document.createElement('img');
+      img.src = icon;
+
 
       getUVI(res.coord.lat, res.coord.lon);
 
-      searchedCityEl.innerHTML = city + " " + moment().format('l') + " ";
+      searchedCityEl.innerHTML = city + " " + "(" + moment().format('l') + ")" + " ";
+      searchedCityEl.appendChild(img);
       currentTempEl.textContent = "Temp: " + temp + "\u00B0" + "F";
       currentWindEl.textContent = "Wind: " + wind + " MPH";
       currentHumidityEl.textContent = "Humidity: " + humidity + "%";
+
+
 
 
   }
@@ -54,9 +60,62 @@ let apiKey = "f35594544b9f9597df35098b60602c39";
 
 
 
+  async function getForecast(cityName) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f35594544b9f9597df35098b60602c39&units=imperial`)
+    
+    let res = await response.json();
+    let forecastZone = document.querySelector("#forecast-stat-zone");
+
+    for (let i = 4; i < 40; i+=8){
+      let tempData = res.list[i];
+     
+
+      let cardEl = document.createElement('div');
+      let dateEl = document.createElement('h5')
+      let iconEl = document.createElement('img')
+      let tempEl = document.createElement('p')
+      let humidityEl = document.createElement('p')
+      let windEl = document.createElement('p')
+
+      dateEl.textContent = new Date (tempData.dt_txt).toLocaleDateString();
+      tempEl.textContent = `Temp: ${tempData.main.temp} \u00B0 F`;
+      humidityEl.textContent = `Humidity: ${tempData.main.humidity}`;
+      windEl.textContent = `Wind: ${tempData.wind.speed} MPH`;
+
+      iconEl.setAttribute("src", `https://openweathermap.org/img/w/${tempData.weather[0].icon}.png`)
+      iconEl.style.height = "50px";
+      iconEl.style.width = "50px";
+      cardEl.classList.add("card", "text-white", "bg-success", "col-2");
+      
+      cardEl.append(dateEl, iconEl, tempEl, windEl, humidityEl);
+
+      forecastZone.append(cardEl);
+
+
+
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Event Listeners
 
   searchBtnEl.addEventListener("click",function() {
-    currentWeather(document.querySelector("#city-input").value);
+    let city = document.querySelector("#city-input").value;
+    console.log(city);
+    currentWeather(city);
+    getForecast(city);
   })
